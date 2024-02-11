1a884279fbda0aa67e3592f0297e645ebfb62450
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-02-11 04:09:28 +0200

    change order of func

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,7 +8,7 @@ import mysql.connector
 import os
 
 
-PII_FIELDS = ("name", "email", "ssn", "phone", "password")
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -20,26 +20,6 @@ def filter_datum(fields: List[str], redaction: str,
                          f'{field}={redaction}{separator}',
                          message)
     return message
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """this is a formatr"""
-        message = filter_datum(self.fields, self.REDACTION,
-                               record.getMessage(), self.SEPARATOR)
-        record.msg = message
-        return super(RedactingFormatter, self).format(record)
 
 
 def get_logger() -> logging.Logger:
@@ -65,3 +45,23 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         host=dbhost,
         database=dbname
     )
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """this is a formatr"""
+        message = filter_datum(self.fields, self.REDACTION,
+                               record.getMessage(), self.SEPARATOR)
+        record.msg = message
+        return super(RedactingFormatter, self).format(record)
