d06a6525bcf5832c766e0dd10e8a401ac8500efb
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-02-11 02:04:19 +0200

    make 4 lines with docs

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,5 @@ import re
 def filter_datum(f: list[str], r: str, m: str, s: str) -> str:
     """filter_datum that returns the log m obfuscated"""
     for f_ in f:
-        f__regex = r'{}=(.*?){}'.format(f_, s)
-        m = re.sub(f__regex, f'{f_}={r}{s}', m)
+        m = re.sub(r'{}=(.*?){}'.format(f_, s), f'{f_}={r}{s}', m)
     return m
