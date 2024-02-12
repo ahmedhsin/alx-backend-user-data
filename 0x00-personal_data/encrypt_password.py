134c5ac324a52ba44901f1a49878f8e87da8ce3c
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-02-12 04:51:48 +0200

    Update encrypt_password.py return anotated

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,6 +9,6 @@ def hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
-def is_valid(hashed_password: bytes, password: str):
+def is_valid(hashed_password: bytes, password: str) -> bool:
     """check if function is valid or not"""
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
