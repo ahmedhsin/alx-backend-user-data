bd9a9dda03eb7a4e01b4b0888c4ae6ddd7409632
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-02-14 23:10:44 +0200

    fix t6

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,7 +13,13 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-
+AUTH_TYPE = getenv('AUTH_TYPE', '')
+if AUTH_TYPE == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif AUTH_TYPE == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 @app.before_request
 def before_request():
@@ -56,12 +62,4 @@ def not_found(error) -> str:
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    AUTH_TYPE = getenv('AUTH_TYPE', '')
-    if AUTH_TYPE == 'auth':
-        from api.v1.auth.auth import Auth
-        auth = Auth()
-    elif AUTH_TYPE == 'basic_auth':
-        from api.v1.auth.basic_auth import BasicAuth
-        auth = BasicAuth()
-
     app.run(host=host, port=port)
