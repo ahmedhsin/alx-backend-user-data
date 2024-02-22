bd451dd5c324d355f5d5fccea8c8b2fe427362d4
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2024-02-22 02:35:56 +0200

    add 8, 9

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,12 +4,18 @@ from db import DB, NoResultFound
 
 from user import Base, User
 import bcrypt
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
     """hash function using bcrypt"""
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     return hashed
+
+
+def _generate_uuid() -> str:
+    """generate a uuid"""
+    return str(uuid.uuid4())
 
 
 class Auth:
@@ -29,3 +35,12 @@ class Auth:
             hashed_password = _hash_password(password)
             user = self._db.add_user(email, hashed_password)
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """check if the credits is valid"""
+        try:
+            user = self._db.find_user_by(email=email)
+            hash_password = user.hashed_password
+            return bcrypt.checkpw(password.encode('utf8'), hash_password)
+        except Exception:
+            return False
